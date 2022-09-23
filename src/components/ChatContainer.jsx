70f02997917f96725f40f82c717dd903bfd059cc
SidathDabare <sidath2007@gmail.com>
2022-09-23 17:24:39 +0200

    add

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -6,8 +6,11 @@ import ListGroup from "react-bootstrap/ListGroup"
 import Overlay from "react-bootstrap/Overlay"
 import { Link } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
-import {sendMessageAction,setSelectedChatMessagesAction} from "../redux/actions/index"
-import { getDataForSpecificChat } from '../lib/apiFunctions'
+import {
+  sendMessageAction,
+  setSelectedChatMessagesAction,
+} from "../redux/actions/index"
+import { getDataForSpecificChat } from "../lib/apiFunctions"
 import { v4 as uuidV4 } from "uuid"
 
 const newSocket = io(process.env.REACT_APP_BE_URL, {
@@ -19,30 +22,39 @@ const ChatContainer = () => {
 
   const loggedInUser = useSelector((state) => state.profile.loggedInUser)
   const selectedChat = useSelector((state) => state.chat.selectedChat)
-  // console.log(loggedInUser._id)
-  // console.log(selectedChat.messages.map((msg) => msg.sender))
+  console.log(selectedChat)
+
+  // const filterdId = selectedChat.messages.filter(
+  //   (msg) => msg.sender === loggedInUser._id
+  // )
+  //console.log(filterdId)
 
   const recipients = selectedChat?.members
-  const messages = useSelector(state => state.chat.selectedChatMessages)
+  const messages = useSelector((state) => state.chat.selectedChatMessages)
   const [show, setShow] = useState(false)
   const [searchMessage, setSearchMessage] = useState(false)
   const [messageText, setMessageText] = useState("")
 
   const target = useRef(null)
 
-  const sendMessage =  () => {
-
+  const sendMessage = () => {
     const messageData = {
       text: messageText,
       room: selectedChat._id,
       sender: loggedInUser._id,
-      time: new Date(Date.now()).getHours() +
-      ":" +
-      new Date(Date.now()).getMinutes(),
+      time:
+        new Date(Date.now()).getHours() +
+        ":" +
+        new Date(Date.now()).getMinutes(),
     }
-     newSocket.emit('send_message', messageData)
-     dispatch(sendMessageAction({_id: uuidV4() ,sender: messageData.sender, content: { text: messageData.text,media: "" }})) 
-     
+    newSocket.emit("send_message", messageData)
+    dispatch(
+      sendMessageAction({
+        _id: uuidV4(),
+        sender: messageData.sender,
+        content: { text: messageData.text, media: "" },
+      })
+    )
   }
   useEffect(() => {
     if (selectedChat?._id) {
@@ -53,24 +65,30 @@ const ChatContainer = () => {
     }
   }, [selectedChat])
 
-  useEffect(()=>{
-    if(selectedChat?._id){getDataForSpecificChat(selectedChat._id).then(data => dispatch(setSelectedChatMessagesAction(data.messages)))}
+  useEffect(() => {
+    if (selectedChat?._id) {
+      getDataForSpecificChat(selectedChat._id).then((data) =>
+        dispatch(setSelectedChatMessagesAction(data.messages))
+      )
+    }
     newSocket.emit("join_room", selectedChat?._id)
-    
-  },[selectedChat, newSocket])
+  }, [selectedChat, newSocket])
 
-  useEffect(()=>{
+  useEffect(() => {
     newSocket.on("receive_message", (receivedMessage) => {
       console.log(selectedChat._id, receivedMessage.room)
-     
-     if(selectedChat._id === receivedMessage.room){
-      dispatch(sendMessageAction({_id: uuidV4() ,sender:receivedMessage.sender, content: { text:receivedMessage.text,media: "" }})) }
-     
+
+      if (selectedChat._id === receivedMessage.room) {
+        dispatch(
+          sendMessageAction({
+            _id: uuidV4(),
+            sender: receivedMessage.sender,
+            content: { text: receivedMessage.text, media: "" },
+          })
+        )
+      }
     })
-  },[])
-
-   
-
+  }, [])
 
   return (
     <>
@@ -151,11 +169,14 @@ const ChatContainer = () => {
           <div>
             {messages?.map((message) => (
               <div className='chat-content-item' key={message._id}>
-                {loggedInUser._id ===
-                selectedChat.messages.map((msg) => msg.sender) ? (
-                  <div className='bg-danger'>{message.content.text}</div>
+                {message.sender !== loggedInUser._id ? (
+                  <div className='bg-danger ml-auto'>
+                    {message.content.text}
+                  </div>
                 ) : (
-                  <div className='bg-warning'>{message.content.text}</div>
+                  <div className='bg-warning mr-auto'>
+                    {message.content.text}
+                  </div>
                 )}
               </div>
             ))}
@@ -177,18 +198,21 @@ const ChatContainer = () => {
                 ? "chat-input col-10 d-flex align-items-center"
                 : "chat-input col-9 d-flex align-items-center"
             }>
-            <form className='w-100' onSubmit={(e)=> 
-              {e.preventDefault()
-               sendMessage()
-               }}>
-              <input type='text' 
-              disabled={ selectedChat? false : true} 
-              value={messageText} 
-              onChange={(e)=> setMessageText(e.target.value)} 
-              placeholder='type...' 
-              className='chat-input-input col-12' />
-              
-              </form>
+            <form
+              className='w-100'
+              onSubmit={(e) => {
+                e.preventDefault()
+                sendMessage()
+              }}>
+              <input
+                type='text'
+                disabled={selectedChat ? false : true}
+                value={messageText}
+                onChange={(e) => setMessageText(e.target.value)}
+                placeholder='type...'
+                className='chat-input-input col-12'
+              />
+            </form>
           </div>
           <div className='col-1 d-flex align-items-center justify-content-around'>
             <i className='bi bi-mic icon-large'></i>
@@ -223,6 +247,6 @@ const ChatContainer = () => {
       </div>
     </>
   )
-      }
-    
+}
+
 export default ChatContainer
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,16 @@
 /** @format */
-import { getDataForLoggedInUser,getChats, getUsers,getAllChatAction } from "../lib/apiFunctions"
+import { getDataForLoggedInUser, getChats } from "../lib/apiFunctions"
 
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
 import React, { useEffect, useState } from "react"
-import { setLoggedInUserAction, setSelectedChatAction, setProfilesAction } from "../redux/actions"
+import {
+  setLoggedInUserAction,
+  setProfilesAction,
+  getAllChatAction,
+} from "../redux/actions"
 import "../styles/HomePage.css"
 import ProfilesContainer from "../components/ProfilesContainer"
 import ChatContainer from "../components/ChatContainer"
-
-
-
 
 const HomePage = () => {
   const token = localStorage.getItem("token")
